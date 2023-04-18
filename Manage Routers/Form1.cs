d1a7c83df03702c2dc897bb17643e674999c4602
Cedric Dominic Lindenau <admin@cloudvibes-empire.de>
2023-04-18 16:02:57 +0200

    Built in cli

--- Manage Routers/Form1.cs
+++ Manage Routers/Form1.cs
@@ -10,9 +10,44 @@ namespace Manage_Routers
         private void Form1_Load(object sender, EventArgs e)
         {
             string[] args = Environment.GetCommandLineArgs();
-            if (args[0] == "--restart")
+            if (args[1] == "--reboot")
             {
                 System.Diagnostics.Process.Start("cmd.exe", "ssh cedri@192.168.178.1 ./reboot.sh");
+                this.Close();
+            }
+            else if (args[1] == "--switch-wifi")
+            {
+                try
+                {
+                    if (args[2] != "fritz" && args[2] != "hotspot") throw new ArgumentException(args[2].ToString() + " is not a valid Wifi-name.";
+                    else System.Diagnostics.Process.Start("cmd.exe", "ssh cedri@10.2.2.1 ./switch_wifi_" + args[2] + ".sh");
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.ToString());
+                    this.Close();
+                    throw;
+                }
+            }
+            else if (args[1] == "htop")
+            {
+                try
+                {
+                    if (args[2] == null && args[2] != "1" && args[2] != "2") throw new ArgumentException(args[2].ToString() + " is not a valid Device ID.");
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.ToString());
+                    this.Close();
+                    throw;
+                }
+                if (args[2] == "1")
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "ssh cedri@192.168.178.1 htop");
+                    this.Close();
+                }
+                System.Diagnostics.Process.Start("cmd.exe", "ssh cedri@10.2.2.1 htop");
+                this.Close();
             }
         }
     }
